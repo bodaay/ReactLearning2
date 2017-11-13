28216a1b739d713539a9e140b03bc0e79e67934c
Khalefa <khalefa@addrek.com>
2017-11-13 11:38:50 +0300

    I wil stop this for now

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'javascript: Sucks big time'},
+        {title: 'KMA book'},
+        {title: 'The Dark Ass'},
+        {title: 'The art of Kotta Book'}
+    ];
+}
+
